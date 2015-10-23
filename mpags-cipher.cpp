50852b2a58beeed4f0c7184f88876abb04dc66df
Sam Gibson <s.j.gibson@warwick.ac.uk>
2015-10-23 16:37:27 +0100

    Early cipher attempt with arguments

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <string>
 
-int main()
+int main(int argc, char* argv[])
 
 {
   char in_char{'x'};
 
   while(std::cin >> in_char)
     {
-      switch (in_char)
-	{
-	  case 
+      //switch (in_char) 
+    }
+
+  for (int i(0); i < argc; ++i)
+    {
+      std::string argument = argv[i];
+      std::cout << argv[i] << std::endl;
     }
 }
